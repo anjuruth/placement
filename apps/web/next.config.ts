cce7463574ccdaed9b28729529b3c99ae099d77a
anjuruth <anjuruth19@gmail.com>
2026-06-10 17:40:26 +0530

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@placement/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
